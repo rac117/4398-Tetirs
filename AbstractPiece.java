a11a85c2c4f697c95b0eadc57a80618ac85bc703
rac117 <30634125+rac117@users.noreply.github.com>
2017-08-01 13:06:24 -0500

    added funct's required in GM class
    -rac117

--- AbstractPiece.java
+++ AbstractPiece.java
@@ -5,7 +5,38 @@ public class AbstractPiece {
 	private int position;
 	private GraphicsObserver graphicsObserver;
 	private int speed;
+	private boolean existence = false;
 
+	public int getColor() {
+		return color;
+	}
+	
+	public void setColor(int c) {
+		color = c;
+	}
+	
+	public int getShape() {
+		return shape;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+	
+	public void initialize() {
+		// calls to createBlock	to setup a piece
+		existence = true;
+	}
+	
+	public void clearPiece(){
+		existence = false;	
+	}
+	
+	public boolean exists() {
+		return existence;	
+	}
+	
+	
 	public void rotate() {
 		// TODO - implement AbstractPiece.rotate
 		throw new UnsupportedOperationException();
